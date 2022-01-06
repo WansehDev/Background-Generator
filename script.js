da2df2125de0670d12d33dbe202f5a6d729973ba
WansehDev <20180014283@my.xu.edu.ph>
2022-01-07 00:10:24 +0800

    Show linear gradient on pageload

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+
+
+
 function setGradient() {
     body.style.background = "linear-gradient(to right," + color1.value + "," + color2.value + ")";
+}
 
+function setCSSLoad() {
+    setGradient()
     css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
 color1.addEventListener("input", setGradient);
+window.addEventListener("load", setCSSLoad);
